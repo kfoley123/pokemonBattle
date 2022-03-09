d68eb9c04a7398448ebfb16f69c4fae478f76fd0
Kortney <kortney@iMac.local>
2022-03-08 21:41:39 -0400

    added battlemenu to HTML, added stylesheet to html, made style for class of hidden. Grab menu and battle menu from html and put them into variables. Toggle class of hidden when we run attack menu function.

--- script.js
+++ script.js
@@ -1,9 +1,28 @@
+const menu = document.querySelector(".mainMenu");
+const battleMenu = document.querySelector(".fightMenu");
 const fightButton = document.querySelector(".fight");
 const pkmnButton = document.querySelector(".pkmn");
 const itemButton = document.querySelector(".item");
 const runButton = document.querySelector(".run");
 
+fightButton.addEventListener("click", attackMenu);
+pkmnButton.addEventListener("click", changePokemon);
+itemButton.addEventListener("click", useItem);
 runButton.addEventListener("click", runAway);
+
+function attackMenu() {
+    //going to open battle move menu
+    menu.classList.toggle("hidden");
+    battleMenu.classList.toggle("hidden");
+}
+
+function changePokemon() {
+    //going to open pokemon team menu
+}
+
+function useItem() {
+    //going to open item menu
+}
 
 function runAway() {
     alert("You can't run from a trainer battle!");
