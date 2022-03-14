af5ace5cf7de9f888078d5396072805eaa3a24a8
Kortney <kortney@iMac.local>
2022-03-14 20:29:06 -0300

    added itemList and partyList variables, when you click on the respective buttons it opens their menus. Made pokemon moveset aray with move objects. In our attack menu function we loop through the moves, for each move we add a button with the class attack and the moves name to the battle menu. Add a click event listener to each button

--- script.js
+++ script.js
@@ -2,8 +2,29 @@ const menu = document.querySelector(".mainMenu");
 const battleMenu = document.querySelector(".fightMenu");
 const fightButton = document.querySelector(".fight");
 const pkmnButton = document.querySelector(".pkmn");
+const partyList = document.querySelector(".partyList");
 const itemButton = document.querySelector(".item");
+const itemList = document.querySelector(".itemList");
 const runButton = document.querySelector(".run");
+
+const moveSet = [
+    {
+        name: "tackle",
+        damage: 10,
+    },
+    {
+        name: "growl",
+        damage: 0,
+    },
+    {
+        name: "scratch",
+        damage: 12,
+    },
+    {
+        name: "bite",
+        damage: 15,
+    },
+];
 
 fightButton.addEventListener("click", attackMenu);
 pkmnButton.addEventListener("click", changePokemon);
@@ -11,19 +32,31 @@ itemButton.addEventListener("click", useItem);
 runButton.addEventListener("click", runAway);
 
 function attackMenu() {
-    //going to open battle move menu
+    moveSet.forEach((move) => {
+        battleMenu.innerHTML += `<button class="attack">${move.name}</button>`;
+    });
+    const moves = document.querySelectorAll(".attack");
+    moves.forEach((move) => {
+        move.addEventListener("click", doMove);
+    });
     menu.classList.toggle("hidden");
     battleMenu.classList.toggle("hidden");
 }
 
 function changePokemon() {
-    //going to open pokemon team menu
+    menu.classList.toggle("hidden");
+    partyList.classList.toggle("hidden");
 }
 
 function useItem() {
-    //going to open item menu
+    menu.classList.toggle("hidden");
+    itemList.classList.toggle("hidden");
 }
 
 function runAway() {
     alert("You can't run from a trainer battle!");
+}
+
+function doMove() {
+    console.log("it works");
 }
